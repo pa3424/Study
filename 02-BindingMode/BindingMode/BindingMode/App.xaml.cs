541c0c76418dcc72455564d18fb588e0cb41426a
YH Park <pa3424@naver.com>
2023-06-19 16:08:33 +0900

    Binding Mode 관련 추가

---
+++ 02-BindingMode/BindingMode/BindingMode/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using BindingMode.ViewModels;
+using BindingMode.Views;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BindingMode
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            Services = ConfigureServices();
+
+            MainWindow mainWindow = null;
+            mainWindow = new MainWindow
+            {
+                WindowStartupLocation = WindowStartupLocation.Manual,
+                WindowState = WindowState.Normal,
+                DataContext = Services.GetService<MainViewModel>()
+            };
+            mainWindow.Show();
+        }
+
+        private IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+            services.AddSingleton<MainViewModel>();
+
+            return services.BuildServiceProvider();
+        }
+
+        public IServiceProvider Services { get; private set; }
+    }
+}
---
+++ 02-BindingMode/BindingMode/BindingMode/ViewModels/MainViewModel.cs
@@ -0,0 +1,91 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace BindingMode.ViewModels
+{
+    public class MainViewModel : ObservableRecipient
+    {
+        public ICommand SetRandomTextCommand { get; private set; }
+
+        public MainViewModel()
+        {
+            SetRandomTextCommand = new RelayCommand<string>(async (text) => await SetRandomText(text));
+        }
+
+        private async Task SetRandomText(string text)
+        {
+            var properties = text.GetType().GetProperties();
+            var randomText = RandomString(new Random().Next(1, 15));
+            switch (text)
+            {
+                case "TwoWayText":
+                    TwoWayText = randomText;
+                    break;
+                case "OneWayText":
+                    OneWayText = randomText;
+                    break;
+                case "OneTimeText":
+                    OneTimeText = randomText;
+                    break;
+                case "OneWayToSourceText":
+                    OneWayToSourceText = randomText;
+                    break;
+                case "DefaultText":
+                    DefaultText = randomText;
+                    break;
+            }
+            await Task.CompletedTask;
+        }
+
+        private string RandomString(int length)
+        {
+            var random = new Random();
+
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        private string twoWayText = "TwoWay_Example";
+        public string TwoWayText
+        {
+            get { return twoWayText; }
+            set { SetProperty(ref twoWayText, value); }
+        }
+
+        private string oneWayText = "OneWay_Example";
+        public string OneWayText
+        {
+            get { return oneWayText; }
+            set { SetProperty(ref oneWayText, value); }
+        }
+
+        private string oneTimeText = "OneTime_Example";
+        public string OneTimeText
+        {
+            get { return oneTimeText; }
+            set { SetProperty(ref oneTimeText, value); }
+        }
+
+        private string oneWayToSourceText = "OneWayToSource_Example";
+        public string OneWayToSourceText
+        {
+            get { return oneWayToSourceText; }
+            set { SetProperty(ref oneWayToSourceText, value); }
+        }
+
+        private string defaultText = "Default_Example";
+        public string DefaultText
+        {
+            get { return defaultText; }
+            set { SetProperty(ref defaultText, value); }
+        }
+    }
+}
