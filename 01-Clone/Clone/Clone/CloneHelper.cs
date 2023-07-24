ed9994201f02a04eee766f5a725cb377d50bc95e
YH Park <pa3424@naver.com>
2023-07-24 14:23:02 +0900

    수정

---
+++ 01-Clone/Clone/Clone/CloneHelper.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Clone
+{
+    public static class CloneHelper
+    {
+        public static T GetClone<T>(this T @this) where T : ICloneable, new()
+        {
+            T clone = new();
+
+            foreach (System.Reflection.PropertyInfo propInfo in typeof(T).GetProperties())
+            {
+                object? propValue = propInfo.GetValue(@this);
+                if (propValue is ICloneable cloneable)
+                {
+                    propInfo.SetValue(clone, cloneable.Clone());
+                }
+                else if (propValue?.GetType().IsValueType ?? false)
+                {
+                    propInfo.SetValue(clone, propValue);
+                }
+            }
+
+            return clone;
+        }
+    }
+}
--- 01-Clone/Clone/Clone/Program.cs
+++ 01-Clone/Clone/Clone/Program.cs
@@ -1,42 +1,54 @@
-﻿using static System.Console;
-// See https://aka.ms/new-console-template for more information
+﻿using Clone;
+using static System.Console;
 
 Person p1 = new Person();
 p1.Age = 32;
 p1.Name = "Sam";
-p1.IdInfo = new IdInfo(3424);
-
-Person p2 = p1.ShallowCopy();
+p1.IdInfo = new IdInfo(0001);
 
 WriteLine("p1 values: ");
 DisplayValues(p1);
-WriteLine("p2 values:");
-DisplayValues(p2);
+
+Person p2 = p1.ShallowCopy();
 
 p2.Age = 42;
 p2.Name = "Frank";
 p2.IdInfo.Id = 7878;
 
-WriteLine("\np1 values: ");
+WriteLine("\nShallow copy");
+WriteLine("p1 values: ");
 DisplayValues(p1);
 WriteLine("p2 values:");
 DisplayValues(p2);
 
 Person p3 = p1.DeepCopy();
 
-p1.Name = "George";
-p1.Age = 23;
-p1.IdInfo.Id = 7117;
+p3.Name = "George";
+p3.Age = 23;
+p3.IdInfo.Id = 7117;
 
-WriteLine("\np1 values: ");
+WriteLine("\nDeep copy");
+WriteLine("p1 values: ");
 DisplayValues(p1);
 WriteLine("p3 values:");
 DisplayValues(p3);
 
+Person p4 = p1.DeepCopyClone();
+
+p4.Name = "YongHyun";
+p4.Age = 30;
+p4.IdInfo.Id = 3424;
+
+WriteLine("\nDeep copy clone");
+WriteLine("p1 values: ");
+DisplayValues(p1);
+WriteLine("p4 values:");
+DisplayValues(p4);
+
 void DisplayValues(Person p) => WriteLine($"Name: {p.Name,-6} Age: {p.Age,-2} Id: {p.IdInfo.Id,-4}");
 
 
-public class Person
+public class Person : ICloneable
 {
     public int Age { get; set; }
     public string Name { get; set; }
@@ -56,12 +68,14 @@ public class Person
         return other;
     }
 
-    public Person Clone() => new Person
+    public Person DeepCopyClone() => new Person
     {
         IdInfo = new IdInfo(IdInfo.Id),
         Age = Age,
         Name = Name,
     };
+
+    public object Clone() => this.GetClone();
 }
 
 public class IdInfo
